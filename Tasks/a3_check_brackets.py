a7e78e9b5a0cdeed2c1810eb14b5150f7bcb17c8
iuliia-eagle <57791709+iuliia-eagle@users.noreply.github.com>
2020-01-16 23:31:36 +0300

    Выполнение четвёртого задания

--- Tasks/a3_check_brackets.py
+++ Tasks/a3_check_brackets.py
@@ -5,5 +5,16 @@ def check_brackets(brackets_row: str) -> bool:
 	:param brackets_row: input string to be checked
 	:return: True if valid, False otherwise
 	"""
-
-	return False
+	n = len(brackets_row)
+	counter = 0
+	for i in range(n):
+		if brackets_row[i] == "(":
+			counter += 1
+		elif brackets_row[i] == ")":
+			counter -= 1
+			if counter < 0:
+				return False
+	if counter == 0:
+		return True
+	else:
+		return False
