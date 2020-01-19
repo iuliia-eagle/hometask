703a6e90bbbbd4e33b8b0f39fcc93a08534f5587
iuliia-eagle <orlova.u.p@yandex.ru>
2020-01-19 22:55:48 +0300

    Задание b1

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -4,10 +4,20 @@ from typing import Any, Sequence, Optional
 def binary_search(elem: Any, arr: Sequence) -> Optional[int]:
 	"""
 	Performs binary search of given element inside of array
-
 	:param elem: element to be found
 	:param arr: array where element is to be found
 	:return: Index of element if it's presented in the arr, None otherwise
 	"""
-	print(elem, arr)
+	n = len(arr)
+	l = 0
+	r = n - 1
+
+	while l <= r:
+		m = int((l + r) / 2)
+		if arr[m] < elem:
+			l = m + 1
+		elif arr[m] > elem:
+			r = m - 1
+		else:
+			return m
 	return None
